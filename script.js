0493b4d6cfa9a41232fffaeec0ec7d425e26138b
Benjamin D Adams <badams888@hotmail.com>
2020-05-01 22:14:58 -0400

    basic js started

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('Add validation!');
+let form = document.querySelector("#parking-form");
+let formIsValid
+
+
+form.addEventListener('submit', function(event){
+    event.preventDefault()
+
+
+let nameTyped = document.querySelector("#name")
+let name = nameTyped.value
+
+
+
+if(name){
+
+let nameStatus = document.getElementById("name-field");
+nameStatus.classList.add("input-valid")
+nameStatus.classList.remove("input-invalid")
+    console.log("truthy af")
+
+} else {
+    let nameStatus = document.getElementById("name-field");
+nameStatus.classList.add("input-invalid")
+nameStatus.classList.remove("input-valid")
+    console.log("falsy af")
+}
+
+})

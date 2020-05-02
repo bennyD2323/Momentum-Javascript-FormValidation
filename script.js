a1f0211f302457379fe9df502d2c7d8eff1979e2
Benjamin D Adams <badams888@hotmail.com>
2020-05-01 22:58:02 -0400

    copypasted placeholder code

--- script.js
+++ script.js
@@ -5,19 +5,14 @@ let formIsValid
 form.addEventListener('submit', function(event){
     event.preventDefault()
 
-
 let nameTyped = document.querySelector("#name")
 let name = nameTyped.value
 
-
-
 if(name){
-
 let nameStatus = document.getElementById("name-field");
 nameStatus.classList.add("input-valid")
 nameStatus.classList.remove("input-invalid")
     console.log("truthy af")
-
 } else {
     let nameStatus = document.getElementById("name-field");
 nameStatus.classList.add("input-invalid")
@@ -26,3 +21,127 @@ nameStatus.classList.remove("input-valid")
 }
 
 })
+// CAR YEAR ETC
+// form.addEventListener('submit', function(event){
+//     event.preventDefault()
+
+// let nameTyped = document.querySelector("#name")
+// let name = nameTyped.value
+
+// if(name){
+// let nameStatus = document.getElementById("name-field");
+// nameStatus.classList.add("input-valid")
+// nameStatus.classList.remove("input-invalid")
+//     console.log("truthy af")
+// } else {
+//     let nameStatus = document.getElementById("name-field");
+// nameStatus.classList.add("input-invalid")
+// nameStatus.classList.remove("input-valid")
+//     console.log("falsy af")
+// }
+
+// })
+// DATE PARKING
+// form.addEventListener('submit', function(event){
+//     event.preventDefault()
+
+// let nameTyped = document.querySelector("#name")
+// let name = nameTyped.value
+
+// if(name){
+// let nameStatus = document.getElementById("name-field");
+// nameStatus.classList.add("input-valid")
+// nameStatus.classList.remove("input-invalid")
+//     console.log("truthy af")
+// } else {
+//     let nameStatus = document.getElementById("name-field");
+// nameStatus.classList.add("input-invalid")
+// nameStatus.classList.remove("input-valid")
+//     console.log("falsy af")
+// }
+
+// })
+
+// # OF DAYS
+// form.addEventListener('submit', function(event){
+//     event.preventDefault()
+
+// let nameTyped = document.querySelector("#name")
+// let name = nameTyped.value
+
+// if(name){
+// let nameStatus = document.getElementById("name-field");
+// nameStatus.classList.add("input-valid")
+// nameStatus.classList.remove("input-invalid")
+//     console.log("truthy af")
+// } else {
+//     let nameStatus = document.getElementById("name-field");
+// nameStatus.classList.add("input-invalid")
+// nameStatus.classList.remove("input-valid")
+//     console.log("falsy af")
+// }
+
+// })
+
+// KREDITNAYA KARTOCHKA
+// form.addEventListener('submit', function(event){
+//     event.preventDefault()
+
+// let nameTyped = document.querySelector("#name")
+// let name = nameTyped.value
+
+// if(name){
+// let nameStatus = document.getElementById("name-field");
+// nameStatus.classList.add("input-valid")
+// nameStatus.classList.remove("input-invalid")
+//     console.log("truthy af")
+// } else {
+//     let nameStatus = document.getElementById("name-field");
+// nameStatus.classList.add("input-invalid")
+// nameStatus.classList.remove("input-valid")
+//     console.log("falsy af")
+// }
+
+// })
+
+// CVV
+// form.addEventListener('submit', function(event){
+//     event.preventDefault()
+
+// let nameTyped = document.querySelector("#name")
+// let name = nameTyped.value
+
+// if(name){
+// let nameStatus = document.getElementById("name-field");
+// nameStatus.classList.add("input-valid")
+// nameStatus.classList.remove("input-invalid")
+//     console.log("truthy af")
+// } else {
+//     let nameStatus = document.getElementById("name-field");
+// nameStatus.classList.add("input-invalid")
+// nameStatus.classList.remove("input-valid")
+//     console.log("falsy af")
+// }
+
+// })
+
+// EXPIRATION DATE
+// form.addEventListener('submit', function(event){
+//     event.preventDefault()
+
+// let nameTyped = document.querySelector("#name")
+// let name = nameTyped.value
+
+// if(name){
+// let nameStatus = document.getElementById("name-field");
+// nameStatus.classList.add("input-valid")
+// nameStatus.classList.remove("input-invalid")
+//     console.log("truthy af")
+// } else {
+//     let nameStatus = document.getElementById("name-field");
+// nameStatus.classList.add("input-invalid")
+// nameStatus.classList.remove("input-valid")
+//     console.log("falsy af")
+// }
+
+// })

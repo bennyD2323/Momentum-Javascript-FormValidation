e047e4e841b7d935ea105d7fa1b127067c7c469b
Benjamin D Adams <badams888@hotmail.com>
2020-05-03 14:56:50 -0400

    fixed error divs

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ if(name){
 let nameStatus = document.getElementById("name-field");
 nameStatus.classList.add("input-valid");
 nameStatus.classList.remove("input-invalid");
-    console.log("truthy af")
+nameError.innerHTML = ""
 } else {
     let nameStatus = document.getElementById("name-field");
 nameStatus.classList.add("input-invalid");
@@ -38,12 +38,12 @@ if(carYear && carMake && carModel){
 let carStatus = document.getElementById("car-field");
 carStatus.classList.add("input-valid");
 carStatus.classList.remove("input-invalid");
-    console.log("truthy af");
+carError.innerHTML = ""
 } else {
     let carStatus = document.getElementById("car-field");
 carStatus.classList.add("input-invalid");
 carStatus.classList.remove("input-valid");
-// carError.innerHTML = "All fields are required!"
+carError.innerHTML = "All fields are required!"
     
 }
 })
@@ -60,12 +60,12 @@ if(startDate) {
 let startDateStatus = document.getElementById("start-date-field");
 startDateStatus.classList.add("input-valid")
 startDateStatus.classList.remove("input-invalid")
-    console.log("truthy af")
+startDateError.innerHTML = ""
 } else {
     let startDateStatus = document.getElementById("start-date-field");
 startDateStatus.classList.add("input-invalid")
 startDateStatus.classList.remove("input-valid")
-    console.log("falsy af")
+startDateError.innerHTML = "Start date is required!"
 }
 })
 
@@ -80,12 +80,13 @@ if(days){
 let daysStatus = document.getElementById("days-field");
 daysStatus.classList.add("input-valid")
 daysStatus.classList.remove("input-invalid")
-    console.log("truthy af")
+daysError.innerHTML = ""
+
 } else {
     let daysStatus = document.getElementById("days-field");
 daysStatus.classList.add("input-invalid")
 daysStatus.classList.remove("input-valid")
-    console.log("falsy af")
+    daysError.innerHTML = "Number of days is required!"
 }
 
 })
@@ -100,12 +101,12 @@ if(cc){
 let ccStatus = document.getElementById("credit-card-field");
 ccStatus.classList.add("input-valid")
 ccStatus.classList.remove("input-invalid")
-    console.log("truthy af")
+ccError.innerHTML = ""
 } else {
     let ccStatus = document.getElementById("credit-card-field");
 ccStatus.classList.add("input-invalid")
 ccStatus.classList.remove("input-valid")
-    console.log("falsy af")
+ccError.innerHTML = "Credit card number is required!"
 }
 
 })
@@ -120,12 +121,12 @@ if(cvv){
 let cvvStatus = document.getElementById("cvv-field");
 cvvStatus.classList.add("input-valid")
 cvvStatus.classList.remove("input-invalid")
-    console.log("truthy af")
+cvvError.innerHTML = ""
 } else {
     let cvvStatus = document.getElementById("cvv-field");
 cvvStatus.classList.add("input-invalid")
 cvvStatus.classList.remove("input-valid")
-    console.log("falsy af")
+cvvError.innerHTML = "CVV numbe is required!"
 }
 
 })
@@ -141,23 +142,48 @@ if(expiration){
 let expirationStatus = document.getElementById("expiration-field");
 expirationStatus.classList.add("input-valid")
 expirationStatus.classList.remove("input-invalid")
-    console.log("truthy af")
+expirationError.innerHTML = ""
 } else {
     let expirationStatus = document.getElementById("expiration-field");
 expirationStatus.classList.add("input-invalid")
 expirationStatus.classList.remove("input-valid")
-    console.log("falsy af")
+expirationError.innerHTML = "Expiration date is required!"
 }
 
 })
 
 //  BEN MESS WITH ERRORDIV SETTING IN THE MORNING!!!!!!!
 let nameError = document.createElement("div")
-let errorDiv = document.querySelector("#name-field")
-errorDiv.appendChild(nameError)
+let nameErrorDiv = document.querySelector("#name-field")
+nameErrorDiv.appendChild(nameError)
 nameError.className = ("nameError");
 
-// let carError = document.createElement("div")
-// let errorDiv = document.querySelector("#car-field")
-// errorDiv.appendChild(carError)
-// carError.className = ("carError");
+let carError = document.createElement("div")
+let carErrorDiv = document.querySelector("#car-field")
+carErrorDiv.appendChild(carError)
+carError.className = ("carError");
+
+let startDateError = document.createElement("div")
+let startDateErrorDiv = document.querySelector("#start-date-field")
+startDateErrorDiv.appendChild(startDateError)
+startDateError.className = ("startDateError");
+
+let daysError = document.createElement("div")
+let daysErrorDiv = document.querySelector("#days-field")
+daysErrorDiv.appendChild(daysError)
+daysError.className = ("daysError");
+
+let ccError = document.createElement("div")
+let ccErrorDiv = document.querySelector("#credit-card-field")
+ccErrorDiv.appendChild(ccError)
+ccError.className = ("ccError");
+
+let cvvError = document.createElement("div")
+let cvvErrorDiv = document.querySelector("#cvv-field")
+cvvErrorDiv.appendChild(cvvError)
+cvvError.className = ("cvvError");
+
+let expirationError = document.createElement("div")
+let expirationErrorDiv = document.querySelector("#expiration-field")
+expirationErrorDiv.appendChild(expirationError)
+expirationError.className = ("expirationError");
